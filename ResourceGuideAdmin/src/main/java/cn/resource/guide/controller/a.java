b0622d3fb7d64e0cf3fd42c6308d7137298efa74
win <win@DESKTOP-FS3SGU9>
2017-06-16 21:31:28 +0800

    Mr.GoldenWan , second commit

--- ResourceGuideAdmin/src/main/java/cn/resource/guide/controller/a.java
+++ ResourceGuideAdmin/src/main/java/cn/resource/guide/controller/a.java
@@ -1,5 +1,14 @@
 package cn.resource.guide.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class a {
 
+	@RequestMapping(value="/hello")
+	public String Hello (){
+		System.out.println("hello");
+		return "index";
+	}
 }
