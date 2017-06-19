9a6a22d57907ec347df1f68cdabb78b96d28066a
win <win@DESKTOP-FS3SGU9>
2017-06-19 14:01:01 +0800

    constant

---
+++ ResourceGuideAdmin/src/main/java/cn/resource/guide/utils/ConstValues.java
@@ -0,0 +1,32 @@
+package cn.resource.guide.utils;
+/*
+ * 
+ * ClassName: ConstValues 
+ * Function: TODO 常量
+ * date: 2016年12月10日 下午9:08:01 
+ * @author 晚进军 Mr.Golden.Wan
+ * @version 
+ * @since JDK 1.7.0_51
+ */
+
+
+public interface ConstValues {
+
+	
+	String ENCONDING = "UTF-8"; // 系统编码方式
+
+	String IP_PORT = "localhost:8080"; // 服务器IP和端口
+
+	String PROJECT_NAME = ""; // 项目名称
+	
+	public static final String LOGINSUCCESS="登录成功";
+	public static final String LOGINFAIL="登录失败";
+
+	public static final String ACCOUNTERROR="账号错误";
+	public static final String PASSERROR="密码错误";
+	public static final String ACCOUNTLOCK="账号被锁定";
+	
+	
+	
+	
+}
