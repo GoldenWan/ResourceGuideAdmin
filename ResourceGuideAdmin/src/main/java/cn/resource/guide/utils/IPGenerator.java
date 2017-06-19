8645370cb65f2950711ea681b8e2953c9d5cd149
asus <asus@ltg>
2017-06-19 14:16:54 +0800

    ip litinggui

---
+++ ResourceGuideAdmin/src/main/java/cn/resource/guide/utils/IPGenerator.java
@@ -0,0 +1,22 @@
+package cn.resource.guide.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IPGenerator {
+
+	public static String getIpAddr(HttpServletRequest req ) {
+		
+		String ip =req.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getRemoteAddr();
+		}
+		return ip;
+	}
+	
+}
