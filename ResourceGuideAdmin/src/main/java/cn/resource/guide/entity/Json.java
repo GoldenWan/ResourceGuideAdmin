02772c8fa262cf12e326c3c70fdd5072471a3c51
win <win@DESKTOP-FS3SGU9>
2017-06-19 13:52:57 +0800

    json bean

---
+++ ResourceGuideAdmin/src/main/java/cn/resource/guide/entity/Json.java
@@ -0,0 +1,28 @@
+package cn.resource.guide.entity;
+
+public class Json {
+	private String msg="";
+	private boolean success=false;
+	private Object object=null;
+	
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public Object getObject() {
+		return object;
+	}
+	public void setObject(Object object) {
+		this.object = object;
+	}
+	
+	
+}
